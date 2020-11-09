2b439f3be1dd69f43e5307ab8ace998f86f6a963
Braedon Kinsey <bkinsey24@biomedscienceacademy.org>
2020-11-09 16:23:52 +0000

    Created and finished it

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Braedon Kinsey
+Period/Core: 2
 
 
 """
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+a_one = input("Enter a adjective: ")
+a_two = input("Enter a adjective: ")
+food = input("Enter a food: ")
+v_one = input("Enter a verb: ")
+v_two = input("Enter a verb: ")
+place_two = input("Enter a place: ")
+
+print(name +" was walking down to the " + place + " when a " + a_one + " monkey jumped from the corner.\n it screamed \"NO MORE MONKEY JUMPING ON THE BED\" and then it ran off.")
+print(f"After all that happened, {name} decided to start heading to {place_two} . When they got there,f it was starting to get dark out. But as they were thinking they saw a {a_two} man doing {v_one}.")
+print("When they got home they sat and started thinking of what to do. They came up with something so funny to do. Just then " + name + " started to " + v_two + " as their dog watched confused after they just watched him eat a whole " + food + " in one bite.")
